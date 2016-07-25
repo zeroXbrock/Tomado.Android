1b6a0efdc17088dd23be03e2641ad501386bc98b
Brock Smedley <brocksmedley@gmail.com>
2016-07-24 17:27:14 -0700

    Sessions list stored in a local SQLite database

--- Tomado/SessionsFragment.cs
+++ Tomado/SessionsFragment.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 using Android.App;
 using Android.Content;
@@ -13,15 +14,46 @@ using Android.Views;
 using Android.Widget;
 using Android.Support.V4.App;
 using Android.Support.V4.View;
+using Android.Util;
+
+using Java.Lang;
+using SQLite;
+
 
 namespace Tomado {
 	public class SessionsFragment : Android.Support.V4.App.Fragment, NewSessionFragment.OnGetNewSessionListener {
+		//view instasnces
 		ListView listViewSessions;
 		Button newSessionButton;
+
 		Android.Net.Uri calendarsUri;
 		
 		//private sessions list for listview
 		List<Session> _sessions;
+
+		//accessor
+		public List<Session> Sessions {
+			get { return _sessions; }
+
+		}
+
+		private string docsFolder;
+		string pathToDatabase;
+
+		public override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			//get database path
+			docsFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+			pathToDatabase = System.IO.Path.Combine(docsFolder, "sessions.db");
+
+			//create database
+			createDatabase(pathToDatabase);
+
+			//instantiate sessions list if first run
+			if (_sessions == null)
+				_sessions = new List<Session>();
+		}
 
 		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			//return base.OnCreateView(inflater, container, savedInstanceState);
@@ -37,19 +69,35 @@ namespace Tomado {
 				//open new session dialog fragment (TODO: implement dialog fragment)
 				NewSessionFragment fragment = new NewSessionFragment();
 				ShowNewSessionDialog();
-
 			};
 
 			//modify layout views
-			GetSessions();
-			PopulateListView();
+			
+			LoadSessionsFromDatabase(pathToDatabase).ContinueWith(t => {
+				Activity.RunOnUiThread(() => { ResetListViewAdapter(); });
+			});
+			
+			
 
 			//return the inflated/modified base layout
 			return rootView;
 		}
 
+		//event handler for NewSessionFragment
+		/// <summary>
+		/// Event handler for NewSessionFragment result
+		/// </summary>
+		/// <param name="dateTime"></param>
+		/// <param name="title"></param>
 		public void OnAddNewSession(DateTime dateTime, string title) {
-			AddSession(dateTime, title);
+			//Add the session to _sessions list.
+			Session session = AddSession(1, dateTime, title);
+
+			//reset the listview adapter
+			ResetListViewAdapter();
+
+			//update the database
+			SaveSessionToDatabase(pathToDatabase, session);
 		}
 
 		void ShowNewSessionDialog() {
@@ -72,22 +120,10 @@ namespace Tomado {
 		}
 
 		//populate listview with sessions
-		private void PopulateListView() {
-			listViewSessions.Adapter = new SessionAdapter(Activity, Sessions);
+		private void ResetListViewAdapter() {
+			listViewSessions.Adapter = new SessionAdapter(Activity, _sessions);
 		}
 
-		//TODO: implement me
-		//retrieve stored sessions from IS
-		private void GetSessions() {
-			if (_sessions == null || _sessions.Count == 0) {
-				_sessions = new List<Session>();
-
-				//Keep these dummy ones here until you get persistent storage working
-				_sessions.Add(new Session(12, 30, 2016, 7, 23, "study math"));
-				_sessions.Add(new Session(2, 0, 2016, 7, 23, "jack off"));
-				_sessions.Add(new Session(3, 30, 2016, 7, 23, "eat 4 sandwiches"));
-			}
-		}
 
 		//TODO: Implement me
 		private List<Session> GetFreeTimeSessions() {
@@ -102,13 +138,31 @@ namespace Tomado {
 			return freeTimeSessions;
 		}
 
-		private void AddSession(DateTime date, string title) {
+		//add session to sessions list and returns the session it created
+		/// <summary>
+		/// Adds a session to the _sessions list.
+		/// </summary>
+		/// <param name="dateTime"></param>
+		/// <param name="title"></param>
+		/// <returns></returns>
+		private Session AddSession(int ID, DateTime dateTime, string title) {
 			//add the new session
-			Session session = new Session(date, title);
+			Session session = new Session(ID, dateTime, title);
 			_sessions.Add(session);
 
-			//reset the listview adapter
-			PopulateListView();
+			return session;
+		}
+		/// <summary>
+		/// Adds a session to the _sessions list.
+		/// </summary>
+		/// <param name="session"></param>
+		/// <returns></returns>
+		private Session AddSession(Session session) {
+			DateTime datetime = new DateTime(session.Year, session.MonthOfYear, session.DayOfMonth, session.StartHour, session.StartMinute, 0);
+			string title = session.Title;
+
+			int ID = _sessions.Count+1;
+			return AddSession(ID, datetime, title);
 		}
 
 		//get cursor to browse calendar events
@@ -144,11 +198,80 @@ namespace Tomado {
 			return cursor;
 		}
 
-		
+		//DEPRECATED
+		//asynchronously saves session list items to database; either adding new items or updating new values on existing items
+		private async void SaveSessionsToDatabase(string pathToDatabase) {
+			//for each session in our sessions list, do an insert/update on the DB
 
-		public List<Session> Sessions {
-			get { return _sessions; }
+			foreach (var s in _sessions) {
+				var result = await insertUpdateData(s, pathToDatabase);
+				//Log.Debug("SaveSessionsToDatabase result", result.ToString());
+			}
+		}
 
+		private async void SaveSessionToDatabase(string pathToDatabase, Session session) {
+			var result = await insertUpdateData(session, pathToDatabase);
+		}
+
+		//asynchronously loads sessions from database to sessions list
+		private async Task<string> LoadSessionsFromDatabase(string pathToDatabase) {
+			//clear sessions list
+			_sessions = new List<Session>();
+
+			///for each item in the database, add it to the sessions list
+			//create/run query
+			var records = getRecords(pathToDatabase);
+
+			try {
+				await records.ContinueWith(t => {
+					foreach (var s in records.Result) {
+						AddSession(s);
+					}
+				});
+				return "Loaded sessions from database";
+			}
+			catch {
+				return "Could not load sessions from database";
+			}
+		}
+
+		private async Task<string> createDatabase(string path) {
+			try {
+				var connection = new SQLiteAsyncConnection(path);
+				await connection.CreateTableAsync<Session>();
+				return "Database created";
+			}
+			catch (SQLiteException ex) {
+				return ex.Message;
+			}
+		}
+
+		private async Task<string> insertUpdateData(Session data, string path) {
+			try {
+				var db = new SQLiteAsyncConnection(path);
+				if (await db.InsertAsync(data) != 0)
+					await db.UpdateAsync(data);
+				return "Single data file inserted or updated";
+			}
+			catch (SQLiteException ex) {
+				return ex.Message;
+			}
+		}
+
+		private async Task<List<Session>> getRecords(string path) {
+			try {
+				var db = new SQLiteAsyncConnection(path);
+				//var records = await db.ExecuteAsync("SELECT * FROM Person");
+
+				var query = db.Table<Session>();
+
+				var records = await query.ToListAsync();
+
+				return records;
+			}
+			catch {
+				return null;
+			}
 		}
 	}
 
